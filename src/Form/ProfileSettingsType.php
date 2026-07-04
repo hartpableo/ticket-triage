c6696dade70cd17bc5767d02529d0ad5495b0647
hartpableo <hart.hpwebdev@gmail.com>
2026-07-04 17:18:48 +0800

    - chore: removed client-side profile saving functionality - chore: cleaned up auth.js

---
+++ src/Form/ProfileSettingsType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileSettingsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('is_verified')
+            ->add('otp')
+            ->add('otp_expiry', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('name')
+            ->add('title')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
