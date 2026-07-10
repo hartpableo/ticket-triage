99e58c86e73f602e7acdf0ff9d9a2ce9a234ed3f
hartpableo <hart.hpwebdev@gmail.com>
2026-07-11 01:02:39 +0800

    - feat: installed knp-time-bundle - feat: ticket details metadata

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
