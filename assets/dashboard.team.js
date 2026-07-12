790b6a31d6a405ee234c83e3d7a37eae28dff4a3
hartpableo <hart.hpwebdev@gmail.com>
2026-07-12 19:22:17 +0800

    wip: remove agent feature

--- assets/dashboard.team.js
+++ assets/dashboard.team.js
@@ -8,4 +8,39 @@ function showAddAgentModal() {
     }
 }
 
+async function removeAgent(userId, userName) {
+    if (confirm(`Are you sure you want to remove ${userName} from this workspace?`)) {
+        try {
+            const response = await fetch("/users", {
+                method: "DELETE",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "App-Csrf-Token": document.querySelector("meta[name=\"ajax-csrf-token\"]")?.getAttribute("content") ?? null
+                },
+                body: JSON.stringify({
+                    userId: userId,
+                    userName: userName
+                })
+            });
+
+            const responseJson = await response.json();
+
+            if (!responseJson.ok) {
+                const errorMsg = responseJson?.message ?? "An error occurred when processing user deletion";
+                console.error(errorMsg);
+                showToast(errorMsg, "error");
+                return;
+            }
+
+            showToast(`${userName} has been removed from this workspace.`, "success");
+        } catch (error) {
+            const errorMsg = `Error: ${error.message}`;
+            console.error(errorMsg);
+            showToast(errorMsg, "error");
+        }
+    }
+}
+
 window.showAddAgentModal = showAddAgentModal;
+window.removeAgent = removeAgent;
